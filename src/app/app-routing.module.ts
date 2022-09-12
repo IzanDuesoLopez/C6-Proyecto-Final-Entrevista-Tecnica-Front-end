3779baee953358de1053f1c505d0ea17c7bb3bf2
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-12 23:41:11 +0200

    routing in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePositionComponent } from './create-position/create-position.component';
+import { LoginComponent } from './login/login.component';
+import { PositionPageComponent } from './position-page/position-page.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'create-position',
+    component: CreatePositionComponent
+  },
+  {
+    path: 'search-page',
+    component: SearchPageComponent
+  },
+  {
+    path: 'position',
+    component: PositionPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
