38a2609f16fb8e28d523bb9f1d8d0bd9bafae3ab
IzanDuesoLopez <izandueso@gmail.com>
2022-09-12 23:46:31 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CandidatesProfileComponent } from './candidates-profile/candidates-profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'aboutus',
+    component:AboutUsComponent
+  },
+  {
+    path:'profile',
+    component:CandidatesProfileComponent
+  },
+  {
+    path:'home',
+    component:LandingPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
