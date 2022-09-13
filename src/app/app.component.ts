841df58661dcf3e3fca9b6f3e4f3e648940bc416
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-13 16:15:44 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +7,4 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'C6-Proyecto-Final-Entrevista-Tecnica-Front-end';
-
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    this.router.navigate(['/home'])
-  }
 }
