21384d386113e28f148927e096c6d162be01c195
IzanDuesoLopez <izandueso@gmail.com>
2022-09-16 18:49:12 +0200

    Busqueda - posicion pendiente

--- src/app/components/position-list/position-list.component.ts
+++ src/app/components/position-list/position-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidatePosition } from 'src/app/models/candidate-position.model';
 import { Position } from 'src/app/models/position.model';
+import { CandidatePositionService } from 'src/app/service/candidate-position.service';
 import { PositionService } from 'src/app/service/position.service';
 
 @Component({
@@ -9,6 +11,8 @@ import { PositionService } from 'src/app/service/position.service';
 })
 export class PositionListComponent implements OnInit {
 
+  clicked:any;
+
   positions?: Position[];
   currentPosition: Position = {};
   currentIndex = -1;
@@ -16,13 +20,24 @@ export class PositionListComponent implements OnInit {
 
   buscar = false;
 
-  constructor(private positionService: PositionService) { }
+  candidatePosition: CandidatePosition = {
+    registry_date: new Date(),
+    test_date: new Date(),
+    completion_date: new Date(),
+    result: 10,
+    id_candidate: 1,
+    id_position: ''
+  };
+  submitted = false;
+
+  constructor(private positionService: PositionService,
+    private candidatePositionService: CandidatePositionService) { }
 
   ngOnInit(): void {
     this.retrievePositions();
   }
 
-  retrievePositions(): void{
+  retrievePositions(): void {
     this.positionService.getAll()
       .subscribe(
         data => {
@@ -63,6 +78,40 @@ export class PositionListComponent implements OnInit {
           console.log(error);
         }
       );
+  }
+
+  saveCandidatePosition(i:any): void {
+    const data = {
+      registry_date: this.candidatePosition.registry_date,
+      test_date: this.candidatePosition.test_date,
+      completion_date: this.candidatePosition.completion_date,
+      result: this.candidatePosition.result,
+      id_candidate: this.candidatePosition.id_candidate,
+      id_position: this.candidatePosition.id_position
+    };
+
+    this.candidatePositionService.createCandidatePosition(data).subscribe(
+      response => {
+        alert(i)
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  newCandidatePosition(): void {
+    this.submitted = false;
+    this.candidatePosition = {
+      registry_date: new Date(),
+      test_date: new Date(),
+      completion_date: new Date(),
+      result: 10,
+      id_candidate: 1,
+      id_position: ''
+    };
   }
 
 }
