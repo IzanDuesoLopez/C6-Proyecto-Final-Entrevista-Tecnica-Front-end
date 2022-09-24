19b5cbd3a82f123c44b40bf214990eadd7948c42
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-09-24 18:35:15 +0200

    visual bug

--- src/app/candidates-profile/candidates-profile.component.ts
+++ src/app/candidates-profile/candidates-profile.component.ts
@@ -111,6 +111,7 @@ export class CandidatesProfileComponent implements OnInit {
           if(this.usuario.username == this.candidatePositionsFinales[i].candidate.username){
             this.candidatePositionsFinales[i].test_date = this.candidatePositionsFinales[i].test_date.substring(0, 10) + ' ' + this.candidatePositionsFinales[i].test_date.substring(11, 16)
             candidatePositionsAux.push(this.candidatePositionsFinales[i])
+            console.log(candidatePositionsAux)
           }
         }
 
